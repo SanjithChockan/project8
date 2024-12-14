52abeb1f1d28d7af574c553d6a9e61e11a683c80
Sanjith Chockan <sanjith.chockan@gmail.com>
2024-12-14 11:11:45 -0600

    added logging out activity

--- webServer.js
+++ webServer.js
@@ -327,20 +327,18 @@ app.post('/admin/logout', async function (req, res) {
   if (!req.session.user_id) {
     return res.status(400).json({ error: 'Not logged in' });
   }
+  
+  await Activity.create({
+    user_id: req.session.user_id,
+    activity_type: 'USER_LOGOUT'
+  });
+
   req.session.destroy( err => {
     if (err) {
       console.error('Logout error:', err);
       return res.status(500).json({ error: 'Internal server error' });
     }
 
-    /*
-    (async () => {
-      await Activity.create({
-        user_id: req.session.user_id,
-        activity_type: 'USER_LOGOUT'
-      });
-    })();
-    */
     return res.json({ success: true });
 
   });
